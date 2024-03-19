65308955aad6083b136fd886898e754d8e86d2a9
Jun <jyjang1222@naver.com>
2024-03-19 15:33:18 +0900

    update

--- Algorithm/src/n2/Sort.java
+++ Algorithm/src/n2/Sort.java
@@ -1,21 +1,33 @@
 package n2;
 
 public class Sort {
-	public static int[] SelectionSortDesc(int[] array) {
+	public static int[] SelectionSortAsc(int[] array) {
 		for (int i = 0; i < array.length - 1; i++) {
-			int max = array[i];
-			int maxIdx = i;
+			int minIdx = i;
 			
 			for (int j = i + 1; j < array.length; j++) {
-				if (array[j] > max) {
-					max = array[j];
-					maxIdx = j;
+				if (array[j] < array[minIdx]) {
+					minIdx = j;
 				}
 			}
 			
 			int temp = array[i];
-			array[i] = max;
-			array[maxIdx] = temp;
+			array[i] = array[minIdx];
+			array[minIdx] = temp;
+		}
+		
+		return array;
+	}
+	
+	public static int[] BubbleSortAsc(int[] array) {
+		for (int i = 0; i < array.length - 1; i++) {
+			for (int j = 0; j < array.length - 1 - i; j++) {
+				if (array[j] > array[j + 1]) {
+					int temp = array[j + 1];
+					array[j + 1] = array[j];
+					array[j] = temp;
+				}
+			}				
 		}
 		
 		return array;
--- Algorithm/src/test/Main.java
+++ Algorithm/src/test/Main.java
@@ -9,9 +9,10 @@ public class Main {
 	public static void main(String[] args) {
 		
 		int[] numList = {345,67,4,56,761,35,1233,0,-1,35,-32,4};
-		numList = Sort.SelectionSortDesc(numList);
-		System.out.println(Arrays.toString(numList));
+//		numList = Sort.SelectionSortAsc(numList);
+		numList = Sort.BubbleSortAsc(numList);
 		
+		System.out.println(Arrays.toString(numList));
 	}
 
 }
